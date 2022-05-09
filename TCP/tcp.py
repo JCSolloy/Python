a67364b6a87de11495203a209d3c2a58d6a496da
Banis <banis@l21142.eegsa.corp>
2022-05-09 14:16:29 -0600

    Pruebas Modbus Python

--- TCP/tcp.py
+++ TCP/tcp.py
@@ -1,14 +1,32 @@
-from pymodbus.client.sync import ModbusTcpClient
-"""
-10.98.59.193
-id = 1
-puerto = 502
-45001
-"""
-client = ModbusTcpClient('127.0.0.1')
-client.write_coil(1, True)
-result = client.read_coils(1,1)
-print(result.bits[0])
-client.close()
+from pyModbusTCP.client import ModbusClient
+c = ModbusClient(host="10.232.3.41", 
+                 port=502,
+                 unit_id=0,
+                 debug=True,
+                 auto_open=True, 
+                 auto_close=True)
+
+regs = c.read_holding_registers(4419, 10)
+
+if regs:
+    print(regs)
+    print('Conexión Modbus correcta')
+else:
+    print("read error")
 
 
+from pyModbusTCP.client import ModbusClient
+c = ModbusClient(host="10.232.3.40", 
+                 port=7004,
+                 unit_id=1,
+                 debug=True,
+                 auto_open=True, 
+                 auto_close=True)
+
+regs = c.read_holding_registers(5000, 10)
+
+if regs:
+    print(regs)
+    print('Conexión Modbus correcta')
+else:
+    print("Error de conexión")
