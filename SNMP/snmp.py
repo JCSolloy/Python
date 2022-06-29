5489bfd79903f95f62477c0fcc71dc102e7eb29c
Banis Solloy <banis@201.91.168.192.telgua.com.gt>
2022-06-29 09:44:54 -0600

    pruebas snmp

---
+++ SNMP/snmp.py
@@ -0,0 +1,53 @@
+"""
+from pysnmp.hlapi import *
+
+iterator = nextCmd(
+    SnmpEngine(),
+    UsmUserData('usr-md5-none', 'authkey1'),
+    UdpTransportTarget(('demo.snmplabs.com', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('IF-MIB'))
+)
+
+for errorIndication, errorStatus, errorIndex, varBinds in iterator:
+
+    if errorIndication:
+        print(errorIndication)
+        break
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        break
+
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
+"""
+
+from pysnmp.hlapi import *
+
+iterator = nextCmd(
+    SnmpEngine(),
+    UsmUserData('admin', 'Administrator'),
+    UdpTransportTarget(('10.232.3.39', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('IF-MIB'))
+)
+
+for errorIndication, errorStatus, errorIndex, varBinds in iterator:
+
+    if errorIndication:
+        print(errorIndication)
+        break
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        break
+
+    else:
+        for varBind in varBinds:
+            print('here 11111')
+            print(' = '.join([x.prettyPrint() for x in varBind]))
+
