ec68ed262b30d9f8ec9c9f62818723f57ded2b5a
JCSolloy <JCSolloy>
2023-03-29 10:28:11 -0600

    add __name__

--- zabbix_scripts/modbus.py
+++ zabbix_scripts/modbus.py
@@ -1,29 +1,30 @@
 from pyModbusTCP.client import ModbusClient
 import sys 
 
+#funcion para realizar las interrogaciones modbus 
+#para ejecutar el script se hace de la siguiente manera "python modbus.py "172.0.0.1" 502 4419 10 0" 
 def Modbus(IP, Puerto, Registro, Cantidad_de_registros, ID_Dispositivo):
     Regs = list()
     try:
-        c = ModbusClient(host=IP,
-                         port=Puerto,
-                         unit_id=ID_Dispositivo,
-                         debug=False,
-                         auto_open=True,
-                         auto_close=True)
-        Regs = c.read_holding_registers(Registro, Cantidad_de_registros)
-        Message = "Comunicación Modbus Correcta"
+         connection = ModbusClient(host=IP, port=Puerto, unit_id=ID_Dispositivo, debug=False)
+         Regs = connection.read_holding_registers(Registro, Cantidad_de_registros)
+         Message = "Comunicación Modbus Correcta"
+         connection.close()
     except:
         Message = "Error de comunicación"
+        connection.close()
     return Regs, Message
 
-IP = str(sys.argv[1])
-Puerto = int(sys.argv[2])
-Registro = int(sys.argv[3])
-Cantidad_de_registros = int(sys.argv[4])
-ID_Dispositivo = int(sys.argv[5])
+if __name__ == '__main__':
 
-print(IP,Puerto,Registro,Cantidad_de_registros,ID_Dispositivo)
+    IP = str(sys.argv[1])
+    Puerto = int(sys.argv[2])
+    Registro = int(sys.argv[3])
+    Cantidad_de_registros = int(sys.argv[4])
+    ID_Dispositivo = int(sys.argv[5])
 
-#test = Modbus("10.232.2.170", 502, 4419,10,0)
-test = Modbus(IP, Puerto, Registro, Cantidad_de_registros, ID_Dispositivo)
-print(test)
+    print(IP,Puerto,Registro,Cantidad_de_registros,ID_Dispositivo)
+
+    #test = Modbus("10.232.2.170", 502, 4419,10,0)
+    test = Modbus(IP, Puerto, Registro, Cantidad_de_registros, ID_Dispositivo)
+    print(test)
