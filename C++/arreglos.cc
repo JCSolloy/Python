8dc5abf9600a72af9ec787da13c08f504b7b281c
Banis <jcsolloy@gmail.com>
2024-02-20 14:53:58 -0600

    arrays

---
+++ C++/arreglos.cc
@@ -0,0 +1,15 @@
+// arrays in c++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arreglo[5] = {1, 2, 3, 4, 5};
+    cout << arreglo[0] << endl;
+    cout << arreglo[1] << endl;
+    cout << arreglo[2] << endl;
+
+    return 0;
+}
+
+
