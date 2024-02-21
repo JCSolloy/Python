6e2315303b2786fd35002d92aecda3bb31d91568
Juan Carlos Solloy <85032245+JCSolloy@users.noreply.github.com>
2024-02-21 08:56:58 +0000

    matrices strings

---
+++ C++/matriztranspuesta.cc
@@ -0,0 +1,17 @@
+// transpuesta de una matriz
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matriz[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
