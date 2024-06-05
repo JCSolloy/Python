dce349b106f1a93c68f1268a99a032066424849c
Banis <jcsolloy@gmail.com>
2024-06-05 02:07:11 -0600

    testing rpa

--- RPA/proyectorpa.py
+++ RPA/proyectorpa.py
@@ -1,4 +1,38 @@
-import pyautogui
+import pyautogui as rpa
+import platform
 
-# Mover el mouse a la posición (100, 100)
-pyautogui.moveTo(100, 100)
+def detect_os():
+    if platform.system() == 'Windows':
+        os = 'Windows'
+    elif platform.system() == 'Darwin':
+        os = 'MacOS'
+    elif platform.system() == 'Linux':
+        os = 'Linux'
+    else:   
+        os = 'Unknown OS'
+    return os
+
+def open_excel():
+    wait = 1
+    if os == 'Windows':
+        rpa.press('winleft')
+        time.sleep(wait)
+        rpa.write('excel')
+        time.sleep(wait)
+        rpa.press('enter')
+    elif os == 'MacOS':
+        rpa.hotkey('command', 'space')
+        time.sleep(wait)
+        rpa.write('excel')
+        time.sleep(wait)
+        rpa.press('enter')
+    elif os == 'Linux':
+        rpa.hotkey('ctrl', 'alt', 't')
+        rpa.write('excel')
+        rpa.press('enter')
+    else:
+        print('Unknown OS')
+
+wait = 1
+os = detect_os()
+open_excel()
