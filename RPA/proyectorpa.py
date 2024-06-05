87178dbb6722e6eb6521170df9337efcd958265f
Banis <jcsolloy@gmail.com>
2024-06-05 02:10:45 -0600

    Testing RPA

--- RPA/proyectorpa.py
+++ RPA/proyectorpa.py
@@ -1,6 +1,7 @@
 import pyautogui as rpa
 import platform
 
+# Detect OS
 def detect_os():
     if platform.system() == 'Windows':
         os = 'Windows'
@@ -12,6 +13,7 @@ def detect_os():
         os = 'Unknown OS'
     return os
 
+# Open Excel
 def open_excel():
     wait = 1
     if os == 'Windows':
@@ -33,6 +35,58 @@ def open_excel():
     else:
         print('Unknown OS')
 
-wait = 1
-os = detect_os()
+# Moverse al inicio de la hoja
 open_excel()
+time.sleep(wait)
+rpa.write('formulario usuarios rpa')
+time.sleep(wait)
+rpa.press('tab')
+time.sleep(wait)
+rpa.press('enter')
+time.sleep(wait*5)
+
+# Moverse al inicio de la hoja
+rpa.hotkey('command', 'left')
+time.sleep(wait)
+rpa.hotkey('command', 'up')
+time.sleep(wait)
+rpa.press('down')
+
+# Open Edge y el link al formulari y moverse al primer campo
+def open_edge():
+    if os == 'Windows':
+        rpa.press('winleft')
+        time.sleep(wait)
+        rpa.write('edge')
+        time.sleep(wait)
+        rpa.press('enter')
+    elif os == 'MacOS':
+        rpa.hotkey('command', 'space')
+        time.sleep(wait)
+        rpa.write('edge')
+        time.sleep(wait)
+        rpa.press('enter')
+        time.sleep(wait)
+        rpa.hotkey('command', 't')
+        time.sleep(wait)
+        rpa.press('enter')
+    elif os == 'Linux':
+        rpa.hotkey('ctrl', 'alt', 't')
+        rpa.write('edge')
+        rpa.press('enter')
+    else:
+        print('Unknown OS')
+
+open_edge()
+time.sleep(wait)
+rpa.write('https://forms.gle/FuJCsL1iZydZoF7w8')
+time.sleep(wait)
+rpa.press('enter')
+time.sleep(wait*5)
+rpa.press('tab')
+time.sleep(wait)
+rpa.press('tab')
+time.sleep(wait)
+rpa.press('tab')
+
+###
